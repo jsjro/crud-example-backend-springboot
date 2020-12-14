a3cc3e07c06bc17ede0f0a85320aa4712b961419
jsjro <racotim@gmail.com>
2020-12-14 10:58:53 +0900

    add swagger 2.9.2, tutorial api

---
+++ src/main/java/io/github/jsjro/crudExampleBackendSpringboot/CrudExampleBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.github.jsjro.crudExampleBackendSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudExampleBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudExampleBackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jsjro/crudExampleBackendSpringboot/CrudExampleBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jsjro.crudExampleBackendSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExampleBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
